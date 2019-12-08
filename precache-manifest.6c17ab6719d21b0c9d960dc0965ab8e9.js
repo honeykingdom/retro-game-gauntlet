f912fe1304f81189bb7eb9e7126fa2093e271939
DmitryScaletta <dmitryscaletta@mail.ru>
2019-12-08 16:45:54 +0300

    Updates

---
+++ precache-manifest.6c17ab6719d21b0c9d960dc0965ab8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c904d29fc4f5dae8a6ab29b399d0d1",
+    "url": "/rgg/index.html"
+  },
+  {
+    "revision": "d6f5b9767c275c24112c",
+    "url": "/rgg/static/js/2.2e8fd156.chunk.js"
+  },
+  {
+    "revision": "62e7a7c49c4306d821de6f45988dcd57",
+    "url": "/rgg/static/js/2.2e8fd156.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db34d27747d9e6dc0063",
+    "url": "/rgg/static/js/main.bc5e435e.chunk.js"
+  },
+  {
+    "revision": "e46076fc645ce5e05363",
+    "url": "/rgg/static/js/runtime-main.4209ab08.js"
+  },
+  {
+    "revision": "94c9ce06489f344484ceaec68e36d3cc",
+    "url": "/rgg/static/media/arrow-right.94c9ce06.svg"
+  }
+]);
