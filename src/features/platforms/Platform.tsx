4221a6513aab8dfe339a522229e41e19d2e0ec66
DmitryScaletta <dmitryscaletta@mail.ru>
2019-12-09 08:42:50 +0300

    feat: add platforms feature

---
+++ src/features/platforms/Platform.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import * as colors from '@material-ui/core/colors';
+
+type Props = {
+  id: string;
+  name: string;
+  releaseDate: string;
+  gamesCount: number;
+  checked: boolean;
+  onNameClick: (id: string) => void;
+  onCheckboxClick: (id: string) => void;
+};
+
+const Platform = ({
+  id,
+  name,
+  gamesCount,
+  checked,
+  onNameClick,
+  onCheckboxClick,
+}: Props) => (
+  <ListItem
+    key={id}
+    button
+    selected={checked}
+    style={{ padding: '0 12px', alignItems: 'stretch' }}
+  >
+    <Checkbox
+      edge="start"
+      color="default"
+      checked={checked}
+      style={{ padding: 4 }}
+      onChange={() => onCheckboxClick(id)}
+    />
+    <ListItemText
+      id={id}
+      primary={name}
+      primaryTypographyProps={{ style: { fontWeight: 'bold' } }}
+      style={{ margin: 0, display: 'flex', alignItems: 'center' }}
+      secondary={`(${gamesCount} games)`}
+      secondaryTypographyProps={{
+        style: { display: 'inline', marginLeft: 4, color: colors.grey[500] },
+      }}
+      onClick={() => onNameClick(id)}
+    />
+  </ListItem>
+);
+
+export default React.memo(Platform);
---
+++ src/features/platforms/Platforms.tsx
@@ -0,0 +1,168 @@
+import React, { useState, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
+import Scrollbar from 'react-scrollbars-custom';
+import { useWindowSize } from 'react-use';
+import { Typography, List, Button, Box, ListItem } from '@material-ui/core';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+
+import analytics from 'utils/analytics';
+import Platform from 'features/platforms/Platform';
+import {
+  selectPlatform,
+  togglePlatform,
+  updateOption,
+  selectedPlatformIdsSelector,
+} from 'features/options/optionsSlice';
+import { isRollingSelector } from 'features/rollGame/rollGameSlice';
+
+import platforms from 'data/platforms.json';
+
+const PlatformsRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+const PlatformsList = styled.div<{ isDisabled: boolean }>`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  ${(p) =>
+    p.isDisabled &&
+    css`
+      opacity: 0.5;
+      pointer-events: none;
+    `};
+`;
+
+const Platforms = () => {
+  const dispatch = useDispatch();
+  const [isVisible, setIsVisible] = useState(true);
+  const isRolling = useSelector(isRollingSelector);
+  const selectedPlatformIds = useSelector(selectedPlatformIdsSelector);
+
+  const windowSize = useWindowSize();
+
+  const handleToggleIsVisible = () => {
+    setIsVisible(!isVisible);
+    analytics.event.ui.platforms(!isVisible);
+  };
+
+  const handleNameClick = useCallback(
+    (id: string) => {
+      dispatch(selectPlatform(id));
+      analytics.event.platforms.platformNameClick(id);
+    },
+    [dispatch],
+  );
+
+  const handleCheckboxClick = useCallback(
+    (id: string) => {
+      dispatch(togglePlatform(id));
+      analytics.event.platforms.platformCheckboxClick(id);
+    },
+    [dispatch],
+  );
+
+  const handleSelectAll = () => {
+    dispatch(
+      updateOption({
+        name: 'selectedPlatformIds',
+        value: platforms.map(({ id }) => id),
+      }),
+    );
+    analytics.event.platforms.selectAll();
+  };
+
+  const handleSelectNone = () => {
+    dispatch(
+      updateOption({
+        name: 'selectedPlatformIds',
+        value: [],
+      }),
+    );
+    analytics.event.platforms.selectNone();
+  };
+
+  const renderPlatformsList = () => (
+    <PlatformsList isDisabled={isRolling}>
+      <Box component="span" mb={1} style={{ marginLeft: 1 }}>
+        <Button
+          color="inherit"
+          size="small"
+          startIcon={
+            <CheckBoxIcon style={{ width: 24, height: 24 }} color="inherit" />
+          }
+          onClick={handleSelectAll}
+        >
+          Select All
+        </Button>
+        <Button
+          color="inherit"
+          size="small"
+          startIcon={
+            <CheckBoxOutlineBlankIcon
+              style={{ width: 24, height: 24 }}
+              color="inherit"
+            />
+          }
+          onClick={handleSelectNone}
+        >
+          Select None
+        </Button>
+      </Box>
+      <Scrollbar style={{ flexGrow: 1 }} native={windowSize.width < 1200}>
+        <List dense disablePadding>
+          {platforms.map(({ id, name, releaseDate, gamesCount }) => (
+            <Platform
+              key={id}
+              id={id}
+              name={name}
+              releaseDate={releaseDate}
+              gamesCount={gamesCount}
+              checked={selectedPlatformIds.includes(id)}
+              onNameClick={handleNameClick}
+              onCheckboxClick={handleCheckboxClick}
+            />
+          ))}
+        </List>
+      </Scrollbar>
+    </PlatformsList>
+  );
+
+  const renderCompactPlatformsList = () => (
+    <Scrollbar native={windowSize.width < 1200}>
+      <List>
+        {platforms
+          .filter(({ id }) => selectedPlatformIds.includes(id))
+          .map(({ id, name }) => (
+            <ListItem key={id} style={{ padding: 0, marginBottom: 8 }}>
+              {name}
+            </ListItem>
+          ))}
+      </List>
+    </Scrollbar>
+  );
+
+  return (
+    <PlatformsRoot>
+      <Box display="flex" justifyContent="space-between" mb={1}>
+        <Typography variant="h5">Platforms</Typography>
+        <Button
+          color="default"
+          variant="contained"
+          size="small"
+          onClick={handleToggleIsVisible}
+        >
+          {isVisible ? 'Hide' : 'Change'}
+        </Button>
+      </Box>
+
+      {isVisible ? renderPlatformsList() : renderCompactPlatformsList()}
+    </PlatformsRoot>
+  );
+};
+
+export default Platforms;
