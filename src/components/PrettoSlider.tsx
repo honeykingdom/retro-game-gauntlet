1b19403d96b9707b3f0bd5af7d0f1b1fd95dcc9d
DmitryScaletta <dmitryscaletta@mail.ru>
2019-12-09 08:43:37 +0300

    feat: add slider component

---
+++ src/components/PrettoSlider.tsx
@@ -0,0 +1,34 @@
+import { Slider, withStyles } from '@material-ui/core';
+import * as colors from '@material-ui/core/colors';
+
+const PrettoSlider = withStyles({
+  root: {
+    color: colors.grey[500],
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus,&:hover,&$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+export default PrettoSlider;
