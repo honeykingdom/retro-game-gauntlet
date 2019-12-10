fffb9baaeb16d71577cca7b19016ff981a686696
DmitryScaletta <dmitryscaletta@mail.ru>
2019-12-10 09:54:08 +0300

    feat: add breakpoints

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,10 @@
-export const GLOBAL_FONT = "Roboto, Helvetica, Arial, sans-serif";
+export const GLOBAL_FONT = 'Roboto, Helvetica, Arial, sans-serif';
 
-export const LS_OPTIONS = "rggOptions";
+export const LS_OPTIONS = 'rggOptions';
+
+export const BREAKPOINTS = {
+  sm: 450,
+  md: 720,
+  lg: 1200,
+  xl: 1840,
+};
