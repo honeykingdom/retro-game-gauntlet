d784258936b042fab6781fddfcf6edfbc48ecf32
DmitryScaletta <dmitryscaletta@mail.ru>
2019-12-10 07:14:07 +0300

    refact: rename grid areas

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,59 +17,59 @@ const theme = createMuiTheme({ palette: { type: 'dark' } });
 const AppRoot = styled.div`
   display: grid;
   min-height: 100vh;
-  grid-template-columns: 16px auto 16px;
   grid-template-rows: 48px 24px auto 24px auto 24px auto 24px 200px;
+  grid-template-columns: 16px auto 16px;
   grid-template-areas:
-    'header header     header'
-    '.      .          .     '
-    '.      roll-game  .     '
-    '.      .          .     '
-    '.      side-right .     '
-    '.      .          .     '
-    '.      side-left  .     '
-    '.      .          .     '
-    'footer footer     footer';
+    'header header header'
+    '.      .      .     '
+    '.      main   .     '
+    '.      .      .     '
+    '.      side-r .     '
+    '.      .      .     '
+    '.      side-l .     '
+    '.      .      .     '
+    'footer footer footer';
 
   @media (min-width: 720px) {
     grid-template-rows: 48px 24px auto 24px auto 24px 48px;
     grid-template-columns: 32px 1fr 24px 1fr 32px;
     grid-template-areas:
-      'header header     header    header     header'
-      '.      .          .         .          .     '
-      '.      roll-game  roll-game roll-game  .     '
-      '.      .          .         .          .     '
-      '.      side-left  .         side-right .     '
-      '.      .          .         .          .     '
-      'footer footer     footer    footer     footer';
+      'header header header header header'
+      '.      .      .      .      .     '
+      '.      main   main   main   .     '
+      '.      .      .      .      .     '
+      '.      side-l .      side-r .     '
+      '.      .      .      .      .     '
+      'footer footer footer footer footer';
   }
 
   @media (min-width: 1200px) {
-    grid-template-columns: auto 340px 40px 400px 40px 340px auto;
     grid-template-rows: 48px 40px auto 40px 48px;
+    grid-template-columns: auto 340px 40px 400px 40px 340px auto;
     grid-template-areas:
-      'header header    header header    header header     header'
-      '.      .         .      .         .      .          .     '
-      '.      side-left .      roll-game .      side-right .     '
-      '.      .         .      .         .      .          .     '
-      'footer footer    footer footer    footer footer     footer';
+      'header header header header header header header'
+      '.      .      .      .      .      .      .     '
+      '.      side-l .      main   .      side-r .     '
+      '.      .      .      .      .      .      .     '
+      'footer footer footer footer footer footer footer';
   }
 
   @media (min-width: 1840px) {
-    grid-template-columns: auto 360px 40px 840px 40px 360px auto;
     grid-template-rows: 64px 40px auto 40px 64px;
+    grid-template-columns: auto 360px 40px 840px 40px 360px auto;
   }
 `;
 const StyledHeader = styled(Header)`
   grid-area: header;
 `;
 const StyledSidebarLeft = styled(Sidebar)`
-  grid-area: side-left;
+  grid-area: side-l;
 `;
 const StyledSidebarRight = styled(Sidebar)`
-  grid-area: side-right;
+  grid-area: side-r;
 `;
 const StyledRollGame = styled(RollGame)`
-  grid-area: roll-game;
+  grid-area: main;
 `;
 const StyledFooter = styled(Footer)`
   grid-area: footer;
