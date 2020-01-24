295381254bdf4530a04219c46a8388c9fc396e9c
DmitryScaletta <dmitryscaletta@mail.ru>
2020-01-25 02:39:42 +0300

    fix: precomplile wheel to increase performance

--- src/features/wheel/drawWheel.ts
+++ src/features/wheel/drawWheel.ts
@@ -15,50 +15,52 @@ const getX = (offset: number, radius: number, angle: number) =>
 const getY = (offset: number, radius: number, angle: number) =>
   offset + radius * Math.cos(angle);
 
-const drawCircle = ({
-  context,
+const precompiledWheel = document.createElement('canvas');
+let wheelHash = '';
+
+const getWheelHash = ({ segments, wheelColors, radius, scale }: DrawState) =>
+  `${segments[0].name}${wheelColors.text}${radius}${scale}`;
+
+const precompileWheel = ({
   segments,
   radius,
-  rotationAngle,
   segmentAngle,
   textFont,
   wheelColors,
   scale,
 }: DrawState) => {
-  if (!context) return;
+  const ctx = precompiledWheel.getContext('2d') as CanvasRenderingContext2D;
 
   const size = radius * 2;
   const textOffset = (20 / 200) * radius;
 
-  context.setTransform(1, 0, 0, 1, 0, 0);
-  context.scale(scale, scale);
-  context.clearRect(0, 0, size, size);
+  precompiledWheel.width = size * scale;
+  precompiledWheel.height = size * scale;
 
-  context.translate(radius, radius);
-  context.rotate(rotationAngle);
-  context.translate(-radius, -radius);
+  ctx.scale(scale, scale);
+  ctx.clearRect(0, 0, size, size);
 
-  context.arc(radius, radius, radius, 0, 2 * Math.PI);
-  context.fillStyle = wheelColors.wheelBackground;
-  context.fill();
+  ctx.arc(radius, radius, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = wheelColors.wheelBackground;
+  ctx.fill();
 
-  context.beginPath();
-  context.arc(radius, radius, radius - LINE_WIDTH / 2, 0, 2 * Math.PI);
-  context.strokeStyle = wheelColors.border;
-  context.lineWidth = LINE_WIDTH;
+  ctx.beginPath();
+  ctx.arc(radius, radius, radius - LINE_WIDTH / 2, 0, 2 * Math.PI);
+  ctx.strokeStyle = wheelColors.border;
+  ctx.lineWidth = LINE_WIDTH;
 
   segments.forEach((_, i) => {
     const lineX = getX(radius, radius, i * segmentAngle);
     const lineY = getY(radius, radius, i * segmentAngle);
 
-    context.moveTo(lineX, lineY);
-    context.lineTo(radius, radius);
+    ctx.moveTo(lineX, lineY);
+    ctx.lineTo(radius, radius);
   });
 
-  context.stroke();
+  ctx.stroke();
 
-  context.font = textFont;
-  context.fillStyle = wheelColors.text;
+  ctx.font = textFont;
+  ctx.fillStyle = wheelColors.text;
 
   segments.forEach((segment, i) => {
     const from = i * segmentAngle;
@@ -70,12 +72,38 @@ const drawCircle = ({
     const textX = getX(radius, radius - textOffset, textAngle);
     const textY = getY(radius, radius - textOffset, textAngle);
 
-    context.save();
-    context.translate(textX, textY);
-    context.rotate(textAngleRotation);
-    context.fillText(segment.name, 0, 4);
-    context.restore();
+    ctx.save();
+    ctx.translate(textX, textY);
+    ctx.rotate(textAngleRotation);
+    ctx.fillText(segment.name, 0, 4);
+    ctx.restore();
   });
+};
+
+const drawCircle = (drawState: DrawState) => {
+  const { context, radius, rotationAngle, scale } = drawState;
+
+  if (!context) return;
+
+  const size = radius * 2;
+
+  context.setTransform(1, 0, 0, 1, 0, 0);
+  context.scale(scale, scale);
+  context.clearRect(0, 0, size, size);
+
+  context.translate(radius, radius);
+  context.rotate(rotationAngle);
+  context.translate(-radius, -radius);
+
+  const currentHash = getWheelHash(drawState);
+
+  if (wheelHash !== currentHash) {
+    wheelHash = currentHash;
+
+    precompileWheel(drawState);
+  }
+
+  context.drawImage(precompiledWheel, 0, 0, size, size);
 };
 
 export default drawCircle;
